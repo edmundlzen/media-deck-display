615107c36f569b658bc8e618fa1c891055952479
edmundlim <edmundlzen@gmail.com>
2023-01-21 11:07:29 +0800

    Added counter

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Say something")
@@ -12,20 +11,25 @@ class MyWindow(Gtk.Window):
         self.box = Gtk.Box(orientation="vertical", spacing=2)
         self.add(self.box)
 
-        self.text = Gtk.Label("Hello")
+        self.currentValue = Gtk.Label(label="0")
 
         self.bottom = Gtk.Box(orientation="horizontal", spacing=2)
-        self.bottom_text1 = Gtk.Label("Left")
-        self.bottom_text2 = Gtk.Label("Right")
-        self.bottom.pack_start(self.bottom_text1, True, True, 0)
-        self.bottom.pack_start(self.bottom_text2, True, True, 0)
+        self.bottom_left_button = Gtk.Button(label="-1")
+        self.bottom_right_button = Gtk.Button(label="+1")
+        self.bottom.pack_start(self.bottom_left_button, True, True, 0)
+        self.bottom.pack_start(self.bottom_right_button, True, True, 0)
 
-        self.box.pack_start(self.text, True, True, 0)
+        self.bottom_left_button.connect("clicked", self.btn_clicked)
+        self.bottom_right_button.connect("clicked", self.btn_clicked)
+
+        self.box.pack_start(self.currentValue, True, True, 0)
         self.box.pack_start(self.bottom, True, True, 0)
 
     def btn_clicked(self, widget):
-        txt = self.ent.get_text()
-        print(txt)
+        if widget == self.bottom_left_button:
+            self.currentValue.set_text(str(int(self.currentValue.get_text()) - 1))
+        elif widget == self.bottom_right_button:
+            self.currentValue.set_text(str(int(self.currentValue.get_text()) + 1))
 
 
 win = MyWindow()
